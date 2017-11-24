dd3780cd7aca908523f069ef348f4b597c314e78
rmoss <bayta47@gmail.com>
2017-11-24 17:00:53 -0600

    completed first training and test evaluation with k nearest neighbors

--- Project/leukemiaCancerClassificationML.py
+++ Project/leukemiaCancerClassificationML.py
@@ -5,12 +5,12 @@ import re
 #***DATA READER*** Takes 3 CSV files and cleans data so that the data items are in the form of learning examples(a two dimensional array of samples, each row corresponds to a sample (patient) and each column corresponds to a feature (gene expression).
 testfile='data_set_ALL_AML_independent.csv'
 trainfile='data_set_ALL_AML_train.csv'
-patient_cancer='actual.csv'
+ct_target='actual.csv'
 
 #import data sets (train and test samples with gene expression values + cancer type labels)
 raw_train = pd.read_csv(trainfile)
 raw_test = pd.read_csv(testfile)
-patient_cancer = pd.read_csv(patient_cancer)
+ct_target = pd.read_csv(ct_target)
 
 # Remove "call" columns not needed from training and test dataframes
 train_keep = [col for col in raw_train.columns if "call" not in col]
@@ -39,4 +39,31 @@ train.columns.names = ['Gene Accession Number']
 test.index.names = ['Patient Samples']
 test.columns.names = ['Gene Accession Number']
 
+# Print head to show data reader works
+print(train.head())
+print(ct_target.head())
+
+
+# ***TRAIN AND TEST***
+# Apply a machine learning algorithm on the gene expression data and evaluate its performance
+
+# Make a test/train/evaluation split of the data.  First combine train and test data into one dataframe
+train = train.append(test)
+
+# For initial test, just using first 50 columns of gene expression data as selected features (X).  Target labelled file is assigned to y, then scikit train_test_split used to manage data 
+X = train#.iloc[:,:50]
+y = ct_target['cancer']
+from sklearn.model_selection import train_test_split
+# to make the experiment replicable we use a constant random_state
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=30, random_state = 253)
+
+# Use a standard ML algorithm on the training set. Using k nearest neighbors from sci-kit learn 
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+# Evaluate the model with the test set.  Using an initial evaluation measure of accuracy. Print accuracy to the screen.
+p = knn.predict(X_test)
+accuracy = sum(p == y_test)/len(y_test)
+print("Accuracy: " + str(accuracy))
 
